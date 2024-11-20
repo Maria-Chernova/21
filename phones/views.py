f857da2c2d797623d8d3c894e437226aaa3fd25e
Maria_Chernova <mari.chernova.06@internet.ru>
2024-11-20 19:47:47 +0300

    Подпись коммита1

--- phones/views.py
+++ phones/views.py
@@ -1,5 +1,3 @@
-from django.shortcuts import render, redirect
-
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Phone
 
@@ -7,19 +5,24 @@ def index(request):
     return redirect('catalog')
 
 def show_catalog(request):
-    # Получаем все телефоны из базы данных
-    phones = Phone.objects.all()
+    sort_option = request.GET.get('sort', 'name')  # Get the sort parameter from the URL
+    if sort_option == 'min_price':
+        phones = Phone.objects.all().order_by('price')  # Sort by price (ascending)
+    elif sort_option == 'max_price':
+        phones = Phone.objects.all().order_by('-price')  # Sort by price (descending)
+    else:
+        phones = Phone.objects.all().order_by('name')  # Sort by name (alphabetical)
+
     template = 'catalog.html'
     context = {
-        'phones': phones  # Передаем список телефонов в контекст
+        'phones': phones
     }
     return render(request, template, context)
 
 def show_product(request, slug):
-    # Получаем телефон по slug, если не найден, возвращаем 404
-    phone = get_object_or_404(Phone, slug=slug)
+    phone = get_object_or_404(Phone, slug=slug)  # Get the phone by slug
     template = 'product.html'
     context = {
-        'phone': phone  # Передаем объект телефона в контекст
+        'phone': phone
     }
     return render(request, template, context)
